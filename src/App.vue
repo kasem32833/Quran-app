9f0bce30f772cf50bed194b632681e7199582d6c
kasem32833 <abulkasem32833@gmail.com>
2022-12-28 11:20:07 +0300

    Quran app using vue npm and vite

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+
+
+<template>
+  <div class="bg-gray-200 min-h-screen">
+    <div class="container bg-white rounded">
+      <h1 class="text-center underline text-2xl mb-4">Al-Quran App</h1>
+      <div class="flex justtify-between">
+        <div class="flex-1">
+          <select name="" id="">
+            <option value="">Slect Sura</option>
+          </select>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
